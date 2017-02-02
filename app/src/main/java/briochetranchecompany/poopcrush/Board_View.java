212e49ef9c2eab51d311e420832cb4076ff2c202
Boulicaut Julien <julien.boulicaut@epita.fr>
2017-02-02 05:15:44 +0000

    amelioration graphic

--- app/src/main/java/briochetranchecompany/poopcrush/Board_View.java
+++ app/src/main/java/briochetranchecompany/poopcrush/Board_View.java
@@ -56,4 +56,48 @@ public class Board_View extends View{
 ;            }
         }
     }
+
+
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec)
+    {
+        int widthMode = MeasureSpec.getMode(widthMeasureSpec);
+        int widthSize = MeasureSpec.getSize(widthMeasureSpec);
+        int heightMode = MeasureSpec.getMode(heightMeasureSpec);
+        int heightSize = MeasureSpec.getSize(heightMeasureSpec);
+
+        int desiredWidth = (int)(0.8f * (float)widthSize) ;
+        int desiredHeight = (int) (0.6f * (float)heightSize);
+
+
+        int width;
+        int height;
+
+        //Measure Width
+        if (widthMode == MeasureSpec.EXACTLY) {
+            //Must be this size
+            width = widthSize;
+        } else if (widthMode == MeasureSpec.AT_MOST) {
+            //Can't be bigger than...
+            width = Math.min(desiredWidth, widthSize);
+        } else {
+            //Be whatever you want
+            width = desiredWidth;
+        }
+
+        //Measure Height
+        if (heightMode == MeasureSpec.EXACTLY) {
+            //Must be this size
+            height = heightSize;
+        } else if (heightMode == MeasureSpec.AT_MOST) {
+            //Can't be bigger than...
+            height = Math.min(desiredHeight, heightSize);
+        } else {
+            //Be whatever you want
+            height = desiredHeight;
+        }
+
+        //MUST CALL THIS
+        setMeasuredDimension(width, height);
+
+    }
 }
