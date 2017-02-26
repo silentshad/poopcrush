092b3897a2fb9f70b973c7ab0b6afadfa90d4b63
hellruba <cesard.anthony@gmail.com>
2017-02-26 23:24:49 +0000

    Menu + end game button

--- app/src/main/java/briochetranchecompany/poopcrush/Menu_activity.java
+++ app/src/main/java/briochetranchecompany/poopcrush/Menu_activity.java
@@ -1,9 +1,13 @@
 package briochetranchecompany.poopcrush;
 
 import android.content.Intent;
+import android.graphics.Color;
+import android.speech.tts.TextToSpeech;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class Menu_activity extends AppCompatActivity {
 
@@ -11,6 +15,8 @@ public class Menu_activity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_activity);
+
+
     }
 
 
@@ -18,5 +24,11 @@ public class Menu_activity extends AppCompatActivity {
     {
         Intent intent = new Intent(this,Poop_Crush.class);
         startActivity(intent);
+
+    }
+
+    public void Quit(View view)
+    {
+        finish();
     }
 }
